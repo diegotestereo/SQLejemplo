f9776f776e95f1651c1de8a997d1cf2e341e3b0a
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2014-06-13 19:46:32 -0300

    casa

---
+++ src/com/example/sqlejemplo/UsuariosSQLiteHelper.java
@@ -0,0 +1,33 @@
+package com.example.sqlejemplo;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class UsuariosSQLiteHelper extends SQLiteOpenHelper{
+	String sqlCreate = "CREATE TABLE Usuarios (codigo INTEGER, nombre TEXT)";
+	public UsuariosSQLiteHelper(Context context, String name,
+			CursorFactory factory, int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		db.execSQL(sqlCreate);
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		 db.execSQL("DROP TABLE IF EXISTS Usuarios");
+		 
+	        //Se crea la nueva versión de la tabla
+	        db.execSQL(sqlCreate);
+		
+		
+	}
+
+}
