4a62096fcc9a7499f1e4076522e9616d58b810cd
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2014-06-13 20:07:05 -0300

    casa

--- src/com/example/sqlejemplo/MainActivity.java
+++ src/com/example/sqlejemplo/MainActivity.java
@@ -11,7 +11,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+// creo lbase de datos DBUsuarios
+      UsuariosSQLiteHelper usdbh = new UsuariosSQLiteHelper(this, "DBUsuarios", null, 1);
         
     }
 
