52f1bfad754561d77cc86de9a709c7879d7aa835
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2014-06-15 00:14:26 -0300

    casa

--- src/com/example/sqlejemplo/MainActivity.java
+++ src/com/example/sqlejemplo/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.sqlejemplo;
 
 import android.app.Activity;
-
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 
@@ -13,6 +13,23 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 // creo lbase de datos DBUsuarios
       UsuariosSQLiteHelper usdbh = new UsuariosSQLiteHelper(this, "DBUsuarios", null, 1);
+      SQLiteDatabase bd =usdbh.getWritableDatabase();
+      //insertare 5 datos   
+      if(bd!=null){
+    	  for(int i=1;i<=5;i++){
+    		  //creo los datos
+    		  
+    		  int codigo=i ;
+    		  String usuario="usuario_"+i;
+    		  //inserto dato en la tabla usuarios
+    		 bd.execSQL("INSERT INTO Usuarios (codigo,nombre) VALUES ("+codigo+","+usuario+")");
+    		 bd.close(); 
+    		  
+    	  }
+    	  
+      } 
+      
+      
         
     }
 
